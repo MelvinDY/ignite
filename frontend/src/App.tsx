261a49a0f9f0e91fe79a230e93590621b51f931a
Giselle Angela Hanjaya Putri <z5491414@ad.unsw.edu.au>
2025-08-31 12:06:46 -0700

    feat: set up routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,28 @@
-import { useState } from 'react'
-import './App.css'
-import UserList from './components/UserList'
-import AddUser from './components/AddUser'
+import { useState } from "react";
+import "./App.css";
+import UserList from "./components/UserList";
+import AddUser from "./components/AddUser";
+import { Route, Routes } from "react-router-dom";
+import { Dashboard } from "./pages/Dashboard";
 
 function App() {
-  const [refreshKey, setRefreshKey] = useState(0)
+  const [refreshKey, setRefreshKey] = useState(0);
 
   const handleUserAdded = () => {
-    setRefreshKey(prev => prev + 1)
-  }
+    setRefreshKey((prev) => prev + 1);
+  };
 
   return (
-    <div className="App">
-      <h1>Full Stack App</h1>
-      <p>React + Vite frontend with TypeScript backend and Supabase</p>
-      <AddUser onUserAdded={handleUserAdded} />
-      <UserList key={refreshKey} />
-    </div>
-  )
+    <>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route
+          path="/add"
+          element={<AddUser onUserAdded={handleUserAdded} />}
+        />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;

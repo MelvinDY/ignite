2abe8bf7f9fe949033024b1fd5555d99c95f88ff
Giselle Angela Hanjaya Putri <z5491414@ad.unsw.edu.au>
2025-08-31 17:28:11 -0700

    feat: add underline if dashboard menu is hovered

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -4,22 +4,22 @@ const Navbar = () => {
   return (
     <nav className="sticky top-0 p-5 backdrop-blur-sm w-full shadow-2xl">
       <ul className="flex gap-10">
-        <li>
+        <li className="hover:underline">
           <Link to="/">Dashboard</Link>
         </li>
-        <li>
+        <li className="hover:underline">
           <Link to="/about">About</Link>
         </li>
-        <li>
+        <li className="hover:underline">
           <Link to="/events">Events</Link>
         </li>
-        <li>
+        <li className="hover:underline">
           <Link to="/login">Login</Link>
         </li>
-        <li>
+        <li className="hover:underline">
           <Link to="/register">Register</Link>
         </li>
-        <li>
+        <li className="hover:underline">
           <Link to="/membership">Membership</Link>
         </li>
       </ul>

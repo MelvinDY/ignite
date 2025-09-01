9e3237313c074ee9e4d5e488aba8e5b20a330ae7
Giselle Angela Hanjaya Putri <z5491414@ad.unsw.edu.au>
2025-08-31 17:36:16 -0700

    fix: change header position to fixed, add smooth scroll to top

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,15 +1,30 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Navbar = () => {
+  const location = useLocation();
+  const handleNavigation = (path: string) => {
+    if (location.pathname === path) {
+      // smooth scroll to top
+      window.scrollTo({ top: 0, behavior: "smooth" });
+      return;
+    }
+  };
+
   return (
-    <nav className="sticky top-0 p-5 backdrop-blur-sm w-full shadow-2xl">
+    <nav className="fixed top-0 p-5 backdrop-blur-sm w-full shadow-2xl">
       <ul className="flex gap-10">
-        <li className="hover:underline">
-          <Link to="/">Dashboard</Link>
-        </li>
-        <li className="hover:underline">
-          <Link to="/about">About</Link>
-        </li>
+        <button
+          className="hover:underline"
+          onClick={() => handleNavigation("/")}
+        >
+          Dashboard
+        </button>
+        <button
+          className="hover:underline"
+          onClick={() => handleNavigation("/about")}
+        >
+          About
+        </button>
         <li className="hover:underline">
           <Link to="/events">Events</Link>
         </li>

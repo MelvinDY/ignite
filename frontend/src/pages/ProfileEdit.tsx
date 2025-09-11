0855aa7af337e807acd305c91de7abb7495228ad
Giselle Angela Hanjaya Putri <z5491414@ad.unsw.edu.au>
2025-09-10 21:05:14 -0700

    fix: change svg icon to lucide-react icon

--- frontend/src/pages/ProfileEdit.tsx
+++ frontend/src/pages/ProfileEdit.tsx
@@ -5,6 +5,7 @@ import { RadioGroup } from "../components/ui/RadioGroup";
 import { TextInput } from "../components/ui/TextInput";
 import { Button } from "../components/ui/Button";
 import { twMerge } from "tailwind-merge";
+import { Pencil } from "lucide-react";
 
 const initialFormData = {
   fullName: "Andrew Garfield",
@@ -33,13 +34,7 @@ const ProfileEdit = () => {
               {/* Profile picture */}
               <div className="w-32 h-32 relative flex justify-center items-center sm:place-self-start">
                 <div className="absolute inset-0 bg-black/30 rounded-full opacity-0 hover:opacity-100 flex justify-center items-center cursor-pointer transition-all">
-                  <svg
-                    className="w-10 h-10 z-10"
-                    viewBox="0 -960 960 960"
-                    fill="#ffffff"
-                  >
-                    <path d="M202.63-202.87h57.24l374.74-374.74-56.76-57-375.22 375.22v56.52Zm-45.26 91q-19.15 0-32.33-13.17-13.17-13.18-13.17-32.33v-102.26q0-18.15 6.84-34.69 6.83-16.53 19.51-29.2l501.17-500.41q12.48-11.72 27.7-17.96 15.21-6.24 31.93-6.24 16.48 0 32.2 6.24 15.71 6.24 27.67 18.72l65.28 65.56q12.48 11.72 18.34 27.56 5.86 15.83 5.86 31.79 0 16.72-5.86 32.05-5.86 15.34-18.34 27.82L324-138.22q-12.67 12.68-29.21 19.51-16.53 6.84-34.68 6.84H157.37Zm597.37-586.39-56.24-56.48 56.24 56.48Zm-148.89 92.41-28-28.76 56.76 57-28.76-28.24Z" />
-                  </svg>
+                  <Pencil />
                 </div>
                 <img
                   className="rounded-full w-32 h-32 object-cover cursor-pointer"

ea33fb218117770c70d671b8d0c70fedd91ed147
Giselle Angela Hanjaya Putri <z5491414@ad.unsw.edu.au>
2025-09-10 23:41:20 -0700

    feat: add different menu to edit profile

--- frontend/src/components/profile-edit/AccountDetailsForm.tsx
+++ frontend/src/components/profile-edit/AccountDetailsForm.tsx
@@ -1,80 +1,54 @@
-import { twMerge } from "tailwind-merge";
-import { Button } from "../ui/Button";
 import { RadioGroup } from "../ui/RadioGroup";
 import { TextInput } from "../ui/TextInput";
-import { useState } from "react";
+import type { EditProfileFormProps } from "./formTypes";
 
-interface AccountDetailsFormProps {
-  initialFormData: {
-    fullName: string;
-    zid: string;
-    isIndonesian: boolean;
-  };
-}
-
-const AccountDetailsForm = ({ initialFormData }: AccountDetailsFormProps) => {
-  const [showSaveButton, setShowSaveButton] = useState(false);
-  const [formData, setFormData] = useState(initialFormData);
-
-  const changeFormData = (field: string, value: string | boolean) => {
-    setFormData((prev) => ({ ...prev, [field]: value }));
-    setShowSaveButton(true);
-  };
-
+const AccountDetailsForm = ({
+  formData,
+  changeFormData,
+}: EditProfileFormProps) => {
   return (
-    <div>
-      <div className="space-y-4">
-        <TextInput
-          id="fullName"
-          label="Full Name"
-          value={formData.fullName}
-          onChange={(value) => changeFormData("fullName", value)}
-          // error={errors.fullName}
-          placeholder="Your full name"
-          required
-        />
+    <div className="space-y-4 w-full">
+      <TextInput
+        id="emailAddress"
+        label="Email Address"
+        value={formData.emailAddress}
+        onChange={(value) => changeFormData("emailAddress", value)}
+        // error={errors.emailAddress}
+        placeholder="Your email address"
+        // className="items-left"
+        required
+      />
 
-        <TextInput
-          id="zid"
-          label="zID"
-          value={formData.zid}
-          onChange={(value) => changeFormData("zid", value)}
-          placeholder="z1234567"
-          required
-        />
+      <TextInput
+        id="fullName"
+        label="Full Name"
+        value={formData.fullName}
+        onChange={(value) => changeFormData("fullName", value)}
+        // error={errors.fullName}
+        placeholder="Your full name"
+        required
+      />
 
-        <RadioGroup
-          name="isIndonesian"
-          label="Are you Indonesian?"
-          value={formData.isIndonesian.toString()}
-          onChange={(value) => changeFormData("isIndonesian", value === "true")}
-          options={[
-            { value: "true", label: "Yes" },
-            { value: "false", label: "No" },
-          ]}
-          required
-        />
+      <TextInput
+        id="zid"
+        label="zID"
+        value={formData.zid}
+        onChange={(value) => changeFormData("zid", value)}
+        placeholder="z1234567"
+        required
+      />
 
-        {/* Save and Cancel Button group, hidden when no changes */}
-        <div
-          className={twMerge(
-            "flex justify-end gap-4 mt-2",
-            !showSaveButton && "hidden"
-          )}
-        >
-          <Button
-            type="button"
-            variant="secondary"
-            children="Cancel"
-            className="w-24"
-            onClick={() => {
-              setFormData(initialFormData);
-              setShowSaveButton(false);
-            }}
-          />
-          <Button type="submit" children="Save" className="w-24" />
-        </div>
-      </div>
+      <RadioGroup
+        name="isIndonesian"
+        label="Are you Indonesian?"
+        value={formData.isIndonesian.toString()}
+        onChange={(value) => changeFormData("isIndonesian", value === "true")}
+        options={[
+          { value: "true", label: "Yes" },
+          { value: "false", label: "No" },
+        ]}
+        required
+      />
     </div>
   );
 };
--- frontend/src/pages/ProfileEdit.tsx
+++ frontend/src/pages/ProfileEdit.tsx
@@ -5,76 +5,130 @@ import { Button } from "../components/ui/Button";
 import { Pencil } from "lucide-react";
 import { AccountDetailsForm } from "../components/profile-edit/AccountDetailsForm";
 import { twMerge } from "tailwind-merge";
+import { DashboardForm } from "../components/profile-edit/DashboardForm";
 
-const initialFormData = {
-  fullName: "Andrew Garfield",
-  zid: "z1234567",
-  isIndonesian: false,
-};
-
-const menuItems = [
-  {
-    label: "Account Details",
-    component: <AccountDetailsForm initialFormData={initialFormData} />,
-  },
-  {
-    label: "Change Password",
-    component: <></>,
-  },
-  {
-    label: "Log Out",
-    component: <></>,
-  },
-];
+interface MenuItem {
+  label: string;
+  component: React.ReactNode;
+}
 
 const ProfileEdit = () => {
   const [menu, setMenu] = useState(0);
+  const [showSaveButton, setShowSaveButton] = useState(false);
+  const [initialFormData, setInitialFormData] = useState({
+    emailAddress: "z1234567@ad.unsw.edu.au",
+    fullName: "Andrew Garfield",
+    zid: "z1234567",
+    isIndonesian: false,
+    bio: "Hello! I'm Andrew, a passionate actor and philanthropist. I love exploring new cultures and cuisines. In my free time, I enjoy hiking and photography.",
+    headline: "Aspiring Actor and Culture Enthusiast",
+    major: "Computer Science",
+    level: "undergrad",
+    program: "Bachelor of Science (Computer Science)",
+  });
+  const [formData, setFormData] = useState(initialFormData);
+
+  const changeFormData = (field: string, value: string | boolean) => {
+    setFormData((prev) => ({ ...prev, [field]: value }));
+    setShowSaveButton(true);
+  };
+
+  const menuItems: MenuItem[] = [
+    {
+      label: "Dashboard",
+      component: (
+        <DashboardForm formData={formData} changeFormData={changeFormData} />
+      ),
+    },
+    {
+      label: "Account Details",
+      component: (
+        <AccountDetailsForm
+          formData={formData}
+          changeFormData={changeFormData}
+        />
+      ),
+    },
+    {
+      label: "Change Password",
+      component: <></>,
+    },
+    {
+      label: "Log Out",
+      component: <></>,
+    },
+  ];
 
   return (
-    <div className="w-full flex flex-col justify-center items-center min-h-screen">
+    <div className="w-full flex flex-col justify-center items-center min-h-screen p-8">
       <BatikBackground />
-      <GlassCard
-        className="w-[90%]"
-        children={
-          <div className="flex flex-col gap-4">
-            <h1 className="font-bold text-3xl place-self-start">My Account</h1>
-            <div className="flex sm:flex-row flex-col justify-between items-center gap-4 p-8">
-              <div className="flex flex-col justify-center items-center gap-4">
-                {/* Profile picture */}
-                <div className="w-32 h-32 relative flex justify-center items-center">
-                  <div className="absolute inset-0 bg-black/30 rounded-full opacity-0 hover:opacity-100 flex justify-center items-center cursor-pointer transition-all">
-                    <Pencil />
-                  </div>
-                  <img
-                    className="rounded-full w-32 h-32 object-cover cursor-pointer"
-                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Andrew_Garfield_Comic-Con_2011_%28Straighten_Crop%29.jpg/250px-Andrew_Garfield_Comic-Con_2011_%28Straighten_Crop%29.jpg"
-                    alt="Profile edit"
-                  />
+      <GlassCard className="w-[90%] h-[calc(100vh-8rem)]">
+        <div className="flex flex-col gap-4 h-full">
+          <h1 className="font-bold text-3xl place-self-start">My Account</h1>
+          <div className="flex sm:flex-row flex-col items-center gap-10 p-8">
+            <div className="flex flex-col justify-center items-center gap-4 h-full">
+              {/* Profile picture */}
+              <div className="w-32 h-32 relative flex justify-center items-center">
+                <div className="absolute inset-0 bg-black/30 rounded-full opacity-0 hover:opacity-100 flex justify-center items-center cursor-pointer transition-all">
+                  <Pencil />
                 </div>
-                <div className="flex flex-col">
-                  {/* Menu */}
-                  {Object.entries(menuItems).map(([_, item], index) => (
-                    <Button
-                      key={index}
-                      type="button"
-                      variant="link"
-                      children={item.label}
-                      className={twMerge(
-                        "mt-2 font-bold text-xl",
-                        menu === index && "after:w-full"
-                      )}
-                      onClick={() => setMenu(index)}
-                    />
-                  ))}
-                </div>
+                <img
+                  className="rounded-full w-32 h-32 object-cover cursor-pointer"
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Andrew_Garfield_Comic-Con_2011_%28Straighten_Crop%29.jpg/250px-Andrew_Garfield_Comic-Con_2011_%28Straighten_Crop%29.jpg"
+                  alt="Profile edit"
+                />
               </div>
-
-              {/* Form */}
+              <div className="flex flex-col">
+                {/* Menu */}
+                {Object.entries(menuItems).map(([_, item], index) => (
+                  <Button
+                    key={index}
+                    type="button"
+                    variant="link"
+                    children={item.label}
+                    className={twMerge(
+                      "mt-2 font-bold text-xl",
+                      menu === index && "after:w-full"
+                    )}
+                    onClick={() => setMenu(index)}
+                  />
+                ))}
+              </div>
+            </div>
+            {/* Form */}
+            <div className="flex flex-1 flex-col justify-start items-start h-[calc(100vh-16rem)] overflow-y-auto p-1">
               {menuItems[menu].component}
+              {/* Save and Cancel Button group, hidden when no changes */}
+              <div
+                className={twMerge(
+                  "flex justify-end gap-4 mt-2",
+                  !showSaveButton && "hidden"
+                )}
+              >
+                <Button
+                  type="button"
+                  variant="secondary"
+                  children="Cancel"
+                  className="w-24"
+                  onClick={() => {
+                    setFormData(initialFormData);
+                    setShowSaveButton(false);
+                  }}
+                />
+                <Button
+                  type="submit"
+                  children="Save"
+                  className="w-24"
+                  onClick={() => {
+                    setInitialFormData(formData);
+                    setShowSaveButton(false);
+                  }}
+                />
+              </div>
             </div>
           </div>
-        }
-      />
+        </div>
+      </GlassCard>
     </div>
   );
 };

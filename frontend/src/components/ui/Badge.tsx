e982ee1069b832bcae165bee702e5c45a4d3f84f
Giselle Angela Hanjaya Putri <z5491414@ad.unsw.edu.au>
2025-09-11 14:02:45 -0700

    style: change Badge color

--- frontend/src/components/ui/Badge.tsx
+++ frontend/src/components/ui/Badge.tsx
@@ -10,8 +10,8 @@ const Badge = ({ text, onRemove, className }: BadgeProps) => {
   return (
     <span
       className={twMerge(
-        `inline-block h-6 bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 
-        rounded dark:bg-blue-200 dark:text-blue-800`,
+        `inline-block h-6 bg-white text-black text-xs font-semibold mr-2 px-2.5 py-0.5 
+        rounded`,
         className
       )}
     >

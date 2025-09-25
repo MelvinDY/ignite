8b0998d1449d885172f2e21673dd32f2c42112d2
Kurniawan <hartadichuadri15@gmail.com>
2025-09-25 17:30:24 +1000

    Implemented routes for listing incoming/ outgoing connection requests

--- backend/src/routes/connections.routes.ts
+++ backend/src/routes/connections.routes.ts
@@ -1,22 +1,27 @@
 import { Router } from "express";
 import jwt from "jsonwebtoken";
-import { cancelConnectionRequest, ConnectionRequestError } from "../services/connections.service";
+import {
+  cancelConnectionRequest,
+  listIncomingConnectionRequest,
+  listOutgoingConnectionRequest,
+} from "../services/connections.service";
+import { ConnectionRequestError } from "../types/ConnectionRequest";
 
 const router = Router();
 
 /**
  * User Story: Cancel a pending connection request
  * POST /connections/requests/:id/cancel
- * 
+ *
  * As a sender, I want to withdraw a pending request.
- * 
+ *
  * Auth: Bearer token required
  * Response (200): { "success": true }
- * 
+ *
  * Validation & Logic:
  * - Must be pending and owned by the caller as sender
  * - Idempotent
- * 
+ *
  * Errors:
  * - 401 { code: NOT_AUTHENTICATED }
  * - 404 { code: NOT_FOUND }
@@ -57,6 +62,46 @@ router.post("/connections/requests/:id/cancel", async (req, res) => {
     console.error("cancel connection request error:", err);
     return res.status(500).json({ code: "INTERNAL" });
   }
+});
+
+/**
+ * User Story 4.2: Listing connection requests
+ */
+router.get("/connections/requests", async (req, res) => {
+  const type = req.query.type as string;
+  const page = parseInt(req.query.page as string) || 1;
+  const pageSize = parseInt(req.query.pageSize as string) || 20;
+  const accessToken = req.headers.authorization?.split(" ")[1];
+
+  let userId: string;
+
+  if (!accessToken) {
+    return res.status(401).json({ code: "NOT_AUTHENTICATED " });
+  }
+
+  try {
+    const decoded = jwt.verify(accessToken, process.env.JWT_SECRET!);
+    userId = decoded.sub as string;
+    if (!userId) {
+      throw new Error("No userId in token");
+    }
+  } catch (err) {
+    return res.status(401).json({ code: "NOT_AUTHENTICATED" });
+  }
+
+  if (type && !["incoming", "outgoing"].includes(type.toString())) {
+    return res.status(400).json({ code: "BAD_REQUEST" });
+  }
+
+  let data;
+
+  if (type == "incoming") {
+    data = await listIncomingConnectionRequest(userId, page, pageSize);
+  } else if (type == "outgoing") {
+    data = await listOutgoingConnectionRequest(userId, page, pageSize);
+  }
+
+  return res.status(200).json(data);
 });
 
 export default router;
